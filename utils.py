9168823444f210287b7df2d605060893ee8aa3e1
Thomas Vaudescal <vaudescal.t@gmail.com>
2023-02-11 14:47:10 -0500

    [ADD] | utils

---
+++ utils.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+import scipy as sp
+from rich import inspect
+import statsmodels.api as sm
+from statsmodels.graphics import tsaplots
